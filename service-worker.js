47e3b0e6afe410a300c6df670ce874914064b0a1
EldritchDaddy <dhiameraxiejasmin@gmail.com>
2026-02-11 18:11:20 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,35 @@
-const CACHE_NAME = "eldritch-v4-cache";
+const CACHE_NAME = "eldritch-v5-cache";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
+// Install
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
+// Activate (delete old caches)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
